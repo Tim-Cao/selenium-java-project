19e2e20a0628598d108c9dfeb54c9314ee0a044e
Yang Cao <yang.cao@liferay.com>
2023-04-16 09:39:51 +0800

    Simplify tests

--- src/test/java/com/liferay/selenium/AccessibilityTest.java
+++ src/test/java/com/liferay/selenium/AccessibilityTest.java
@@ -1,70 +1,37 @@
 package com.liferay.selenium;
 
-import com.deque.html.axecore.results.Results;
-import com.deque.html.axecore.results.Rule;
-import com.deque.html.axecore.selenium.AxeBuilder;
-import com.deque.html.axecore.selenium.AxeReporter;
 import com.liferay.selenium.path.Link;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.WebDriver;
-
-import java.util.Arrays;
-import java.util.List;
 
 import static driver.BrowserDriver.*;
 
 public class AccessibilityTest {
-    WebDriver webDriver;
 
     @BeforeEach
     public void setUp() {
-        webDriver = start();
-        webDriver.get("https://www.liferay.com/");
+        start();
+        open("https://www.liferay.com/");
     }
 
     @AfterEach
     public void tearDown() {
-        webDriver.quit();
+        close();
     }
 
     @Test
     public void scanHomePage() throws Exception {
         waitForElementPresent(Link.LEARN_MORE);
 
-        AxeBuilder axeBuilder = new AxeBuilder();
-
-        axeBuilder.withTags(Arrays.asList("wcag2a", "wcag2aa", "wcag21a", "wcag21aa"));
-
-        Results results = axeBuilder.analyze(webDriver);
-
-        List<Rule> rules = results.getViolations();
-
-        if (!rules.isEmpty()) {
-            AxeReporter.getReadableAxeResults("violations", webDriver, rules);
-
-            throw new Exception(AxeReporter.getAxeResultString());
-        }
+        scanPage();
     }
 
     @Test
-    public void scanColorContrast() throws Exception {
+    public void scanHomePageColorContrast() throws Exception {
         waitForElementPresent(Link.LEARN_MORE);
 
-        AxeBuilder axeBuilder = new AxeBuilder();
-
-        axeBuilder.withTags(Arrays.asList("wcag2a", "wcag2aa", "wcag21a", "wcag21aa"));
-
-        Results results = axeBuilder.analyze(webDriver);
-
-        List<Rule> rules = results.getIncomplete();
-
-        if (!rules.isEmpty()) {
-            AxeReporter.getReadableAxeResults("incomplete", webDriver, rules);
-
-            throw new Exception(AxeReporter.getAxeResultString());
-        }
+        scanColorContrast();
     }
 
 }
--- src/test/java/com/liferay/selenium/SignInTest.java
+++ src/test/java/com/liferay/selenium/SignInTest.java
@@ -5,31 +5,25 @@ import com.liferay.selenium.path.Icon;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.WebDriver;
 
 import static driver.BrowserDriver.*;
 
 public class SignInTest {
 
-    WebDriver webDriver;
-
     @BeforeEach
     public void setUp() {
-        webDriver = start();
-        webDriver.get("http://localhost:8080");
+        start();
+        open("http://localhost:8080");
     }
 
     @AfterEach
     public void tearDown() {
-        webDriver.quit();
+        close();
     }
 
     @Test
     public void login() {
-        SignInPage.clickSignInLink();
-        SignInPage.setEmailAddress("test@liferay.com");
-        SignInPage.setPassword("test");
-        SignInPage.clickSignInButton();
+        SignInPage.signIn("test@liferay.com", "test");
 
         assertElementVisible(Icon.USER_AVATAR);
     }
