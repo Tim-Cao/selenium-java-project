e6c889dac87c89f3106838c9bc95f1a49dd60029
Yang Cao <395500850@qq.com>
2024-04-12 16:15:13 +0800

    Add test for search keywords

---
+++ src/test/java/com/liferay/selenium/SearchTest.java
@@ -0,0 +1,30 @@
+package com.liferay.selenium;
+
+import com.liferay.selenium.page.SearchPage;
+import org.junit.*;
+
+import static driver.BrowserDriver.*;
+
+public class SearchTest {
+
+    @Before
+    public void setUp() {
+        start();
+    }
+
+    @After
+    public void tearDown() {
+        close();
+    }
+
+    @Test
+    public void searchByKeywords() {
+        open("https://www.baidu.com");
+
+        SearchPage.type("//input[contains(@class,'s_ipt')]", "今日热点");
+        SearchPage.clickSearchButton("//input[@type='submit']");
+
+        assertElementVisible("//a[normalize-space()='今日热点的最新相关信息']");
+    }
+
+}
