082cc381120dae9476220a743b41570abad6ae27
Yang Cao <395500850@qq.com>
2024-04-12 16:40:23 +0800

    Add dragAndDrop

---
+++ src/test/java/com/liferay/selenium/interactions/DragAndDrop.java
@@ -0,0 +1,17 @@
+package com.liferay.selenium.interactions;
+
+import static driver.BrowserDriver.*;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+
+public class DragAndDrop {
+    public static void dragAndDropOnElement(String source, String target) {
+        WebElement sourceElement = getElement(source);
+        WebElement targetElement = getElement(target);
+
+        new Actions(getDriver())
+                .dragAndDrop(sourceElement, targetElement)
+                .perform();
+    }
+
+}
