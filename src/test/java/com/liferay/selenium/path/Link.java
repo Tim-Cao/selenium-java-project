7e041869d59c965d2ce4c42be997c497f1286083
Yang Cao <395500850@qq.com>
2024-04-14 16:27:59 +0800

    Modify locator for reusable

--- src/test/java/com/liferay/selenium/path/Link.java
+++ src/test/java/com/liferay/selenium/path/Link.java
@@ -1,6 +1,5 @@
 package com.liferay.selenium.path;
 
 public class Link {
-    public static final String LEARN_MORE = "//a[contains(@class,'btn--primary')][contains(.,'Learn More')]";
-    public static final String SIGN_IN = "//a[contains(.,'Sign In')]";
+    public static final String SIGN_IN = "//a[contains(.,'Sign In') or contains(.,'Sign in')]";
 }
